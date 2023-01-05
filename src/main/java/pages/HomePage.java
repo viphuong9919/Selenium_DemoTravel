5fda9ea2d1c0e6ea210af250470a764888d25158
viphuong9919 <75656116+viphuong9919@users.noreply.github.com>
2023-01-05 09:34:58 +0700

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -9,18 +9,28 @@ import org.openqa.selenium.WebElement;
 public class HomePage {
     private WebDriver driver;
     private By txtUsername = By.xpath("(//div[@class='section-heading']//h2)[1]");
+    private By spanUsername = By.xpath("(//div[@class='section-heading']//h2//span)[1]");
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
-    public void verifyLoginSuccessful(String username) {
+    public void verifyLoginSuccessful() {
         WebElement getTextUsername = driver.findElement(txtUsername);
+        WebElement getUsername = driver.findElement(spanUsername);
         String textUsername = null;
+        String name = null;
             getTextUsername.isDisplayed();
             textUsername = getTextUsername.getText();
+            name = getUsername.getText();
             System.out.println(textUsername);
-        if(textUsername.equals(username)){
-            System.out.println("pass");
-        } else {System.out.println("fail");}
+        System.out.println(name);
+        if (textUsername.contains("Hi,"+name+"Welcome Back")){
+            System.out.println("Expected text is obtained");
+        }
+        else{
+
+            System.out.println("Expected text is not obtained");
+
+        }
     }
 }
 
