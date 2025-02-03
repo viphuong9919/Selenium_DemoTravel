8601c531e70f221d0ad109a2e1671a0a2fcde92a
viphuong9919 <75656116+viphuong9919@users.noreply.github.com>
2025-02-03 21:49:16 +0700

    Update Ex1.java

--- src/main/java/Ex1.java
+++ src/main/java/Ex1.java
@@ -6,23 +6,19 @@ import java.util.concurrent.TimeUnit;
 
 public class Ex1    {
     public static void main(String[] args) throws ParseException {
-        // Định dạng thời gian
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd");
         Calendar c1 = Calendar.getInstance();
         Calendar c2 = Calendar.getInstance();
 
-        // Định nghĩa 2 mốc thời gian ban đầu
         Date startDate = dateFormat.parse("20170201");
         Date closeDate = dateFormat.parse("20211224");
 
         c1.setTime(startDate);
         c2.setTime(closeDate);
 
-        // Công thức tính tổng số ngày từ ngày bắt đầu đến ngày kết thúc
         long diff = closeDate.getTime() - startDate.getTime();
         System.out.println ("Tổng số ngày từ ngày bắt đầu đến ngày kết thúc  : " + TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
 
-        // Công thức tính ngày closeDate khi đầu vào cho startDate và số tháng muốn cộng thêm
         c1.add(Calendar.MONTH, 1);
         System.out.println("Tháng được tăng thêm 1 tháng (Sử dụng add)  : "
                 + dateFormat.format(c1.getTime()));
